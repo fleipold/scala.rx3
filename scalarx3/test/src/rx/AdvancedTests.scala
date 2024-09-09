144de2c0c47b10e1f536644f8be8f6d027b1c7c7
Felix Leipold <felix.leipold@gmail.com>
2024-09-10 00:14:49 +0200

    Restructuring build and add tests

---
+++ scalarx3/test/src/rx/AdvancedTests.scala
@@ -0,0 +1,316 @@
+//package rx
+//
+//import utest.*
+//
+//object AdvancedTests extends TestSuite{
+//
+//  //Using a static scope for some tests to check the more common use case of the Rx macro
+//  object MacroDoesRightThing {
+//    var top1Count = 0
+//    var top2Count = 0
+//    var inner1Count = 0
+//    var inner2Count = 0
+//
+//    def chk() = {
+//      assert(inner1Count == inner2Count)
+//      assert(top1Count == top2Count)
+//    }
+//
+//    val top = Var(List(1,2,3))
+//    val other = Var(1)
+//
+//    def inner1(num: Int)(implicit topCtx: Ctx.Owner) = Rx.build { (owner, data) =>
+//      inner1Count += 1
+//      other()(data)
+//    }(topCtx)
+//
+//    def inner2(num: Int)(implicit topCtx: Ctx.Owner) = Rx {
+//      inner2Count += 1
+//      other()
+//    }
+//
+//    val things1 = Rx {
+//      top1Count += 1
+//      top().map(a => inner1(a))
+//    }
+//
+//    val things2 = Rx {
+//      top2Count += 1
+//      top().map(a => inner2(a))
+//    }
+//  }
+//
+//  object SafeTrait {
+//    trait RxTrait {
+//      implicit def ctx: Ctx.Owner // leave ctx abstract
+//      val aa = Var(1)
+//      lazy val meh = Rx { aa() }
+//    }
+//
+//    object RxObj extends RxTrait {
+//      //override implicit val ctx = Ctx.Owner.Unsafe
+//      override implicit val ctx = Ctx.Owner.safe()
+//      val objRx = Rx { meh() }
+//    }
+//
+//    class RxClass extends RxTrait {
+//      //compile time error:
+//      //override implicit val ctx = RxCtx.safe()
+//      override implicit val ctx = Ctx.Owner.Unsafe
+//      val classRx = Rx { meh() }
+//    }
+//
+//    //One other way RxCtx.safe() can be used
+//    class RxClass2()(implicit ZZZ: Ctx.Owner) extends RxTrait {
+//      override val ctx = Ctx.Owner.safe()
+//    }
+//
+//    //Although this would be more normal
+//    class RxClass3()(implicit override val ctx: Ctx.Owner) extends RxTrait {
+//    }
+//  }
+//
+//  def tests = utest.Tests {
+//    "perf" - {
+////      'init{
+////        val start = System.currentTimeMillis()
+////        var n = 0
+////        while(System.currentTimeMillis() < start + 10000){
+////          val (a, b, c, d, e, f) = Utils.initGraph
+////          n += 1
+////        }
+////        n
+////      }
+////      'propagations{
+////        val (a, b, c, d, e, f) = Utils.initGraph
+////        val start = System.currentTimeMillis()
+////        var n = 0
+////
+////        while(System.currentTimeMillis() < start + 10000){
+////          a() = n
+////          n += 1
+////        }
+////        n
+////      }
+//    }
+//    "nesting" - {
+//      "nestedRxs" - {
+//        import Ctx.Owner.Unsafe.*
+//
+//        val a = Var(1)
+//        val b = Rx{
+//          Rx{ a() } -> Rx{ math.random }
+//        }
+//        val r = b.now._2.now
+//        a() = 2
+//        assert(b.now._2.now == r)
+//      }
+//      "macroDoesTheRightThing" - {
+//        import MacroDoesRightThing.*
+//        chk()
+//        other() = other.now + 1
+//        top() = List(3,2,1)
+//        top() = List(2,2,2)
+//        top() = List(1,2,3)
+//        other() = other.now + 1
+//        chk()
+//      }
+//      "safeTrait" - {
+//        import SafeTrait.*
+//        RxObj.aa() = 3
+//        assert(RxObj.meh.now == 3)
+//        assert(RxObj.objRx.now == 3)
+//
+//        val instance = new RxClass
+//        assert(instance.meh.now == 1)
+//        instance.aa() = 5
+//        assert(instance.classRx.now == 5)
+//
+//        val instance2 = new RxClass2()(Ctx.Owner.Unsafe)
+//        assert(instance2.meh.now == 1)
+//
+//        val instance3 = new RxClass3()(Ctx.Owner.Unsafe)
+//        instance3.aa() = 42
+//        assert(instance3.meh.now == 42)
+//      }
+//      "recalc" - {
+//        import Ctx.Owner.Unsafe.*
+//
+//        var source = 0
+//        val a = Rx{
+//          source
+//        }
+//        var i = 0
+//        val o = a.trigger{
+//          i += 1
+//        }
+//        assert(i == 1)
+//        assert(a.now == 0)
+//        source = 1
+//        assert(a.now == 0)
+//        a.recalc()
+//        assert(a.now == 1)
+//        assert(i == 2)
+//      }
+//      "multiset" - {
+//        import Ctx.Owner.Unsafe.*
+//
+//        val a = Var(1)
+//        val b = Var(1)
+//        val c = Var(1)
+//        val d = Rx{
+//          a() + b() + c()
+//        }
+//        var i = 0
+//        val o = d.trigger{
+//          i += 1
+//        }
+//        assert(i == 1)
+//        assert(d.now == 3)
+//        a() = 2
+//        assert(i == 2)
+//        assert(d.now == 4)
+//        b() = 2
+//        assert(i == 3)
+//        assert(d.now == 5)
+//        c() = 2
+//        assert(i == 4)
+//        assert(d.now == 6)
+//
+//        Var.set(
+//          a -> 3,
+//          b -> 3,
+//          c -> 3
+//        )
+//
+//        assert(i == 5)
+//        assert(d.now == 9)
+//
+//        Var.set(
+//          Seq(
+//            a -> 4,
+//            b -> 5,
+//            c -> 6
+//          ):_*
+//        )
+//
+//        assert(i == 6)
+//        assert(d.now == 15)
+//      }
+//      "webPage" - {
+//        import Ctx.Owner.Unsafe.*
+//
+//        var fakeTime = 123
+//        trait WebPage{
+//          def fTime = fakeTime
+//          val time = Var(fTime)
+//          def update(): Unit  = time() = fTime
+//          val html: Rx[String]
+//        }
+//        class HomePage extends WebPage {
+//          val html = Rx{"Home Page! time: " + time()}
+//        }
+//        class AboutPage extends WebPage {
+//          val html = Rx{"About Me, time: " + time()}
+//        }
+//
+//        val url = Var("www.mysite.com/home")
+//        val page = Rx{
+//          url() match{
+//            case "www.mysite.com/home" => new HomePage()
+//            case "www.mysite.com/about" => new AboutPage()
+//          }
+//        }
+//        assert(page.now.html.now == "Home Page! time: 123")
+//
+//        fakeTime = 234
+//        page.now.update()
+//        assert(page.now.html.now == "Home Page! time: 234")
+//
+//        fakeTime = 345
+//        url() = "www.mysite.com/about"
+//        assert(page.now.html.now == "About Me, time: 345")
+//
+//        fakeTime = 456
+//        page.now.update()
+//        assert(page.now.html.now == "About Me, time: 456")
+//      }
+//    }
+//    "higherOrderRxs" - {
+//      import Ctx.Owner.Unsafe.*
+//
+//      val a = Var(1)
+//      val b = Var(2)
+//      val c = Rx(Rx(a() + b()) -> (a() - b()))
+//
+//      assert(
+//        a.downStream.size == 2,
+//        b.downStream.size == 2,
+//        c.now._1.now == 3,
+//        c.now._2 == -1
+//      )
+//
+//      a() = 2
+//
+//      assert(
+//        a.downStream.size == 2,
+//        b.downStream.size == 2,
+//        c.now._1.now == 4,
+//        c.now._2 == 0
+//      )
+//
+//      b() = 3
+//
+//      assert(
+//        a.downStream.size == 2,
+//        b.downStream.size == 2,
+//        c.now._1.now == 5,
+//        c.now._2 == -1
+//      )
+//    }
+//
+//    "leakyRx" - {
+//      var testY = 0
+//      var testZ = 0
+//      val a = Var(10)
+//
+////      Correct way to implement a def: Rx[_]
+//      def y()(implicit zzz: Ctx.Owner) = Rx { testY += 1; a() }
+//
+//      //This way will leak an Rx (ie exponential blow up in cpu time), but is not caught at compile time
+//      def z() = Rx.unsafe { testZ += 1; a() }
+//
+//      val yy = Rx.unsafe { a() ; for (i <- 0 until 100) yield y() }
+//      val zz = Rx.unsafe { a() ; for (i <- 0 until 100) yield z() }
+//      a() = 1
+//      a() = 2
+//      a() = 3
+//      a() = 4
+//      assert(testY == 500)
+//      assert(testZ == 1500)
+//    }
+//
+//    "leakyObs" - {
+//      var testY = 0
+//      var testZ = 0
+//      val a = Var(10)
+//
+//      //Correct way to implement a def: Obs
+//      def y()(implicit zzz: Ctx.Owner) = a.trigger(testY += 1)
+//
+//
+//      //This way will leak the Obs (ie exponential blow up in cpu time), but is not caught at compile time
+//      def z() = a.trigger(testZ += 1)(Ctx.Owner.Unsafe)
+//
+//      val yy = Rx.unsafe { a() ; for (i <- 0 until 100) yield y() }
+//      val zz = Rx.unsafe { a() ; for (i <- 0 until 100) yield z() }
+//      a() = 1
+//      a() = 2
+//      a() = 3
+//      a() = 4
+//      assert(testY == 500)
+//      assert(testZ == 1500)
+//    }
+//
+//  }
+//}
---
+++ scalarx3/test/src/rx/BasicTests.scala
@@ -0,0 +1,304 @@
+package rx
+import util.{Failure, Success}
+
+import utest._
+
+object BasicTests extends TestSuite{
+
+  //We dont care about potential Rx leaks in BasicTest
+  import Ctx.Owner.Unsafe._
+
+  def tests = utest.Tests {
+    "sigTests" - {
+      "basic" - {
+        "rxHelloWorld" - {
+          val a = Var(1); val b = Var(2)
+          val c = Rx.build{(o: Ctx.Owner, d: Ctx.Data) => a()(d) + b()(d) }
+          assert(c.now == 3)
+          a() = 4
+          assert(c.now == 6)
+        }
+        "ordering" - {
+          var changes = ""
+          val a = Var(1)
+          val b = Rx{ changes += "b"; a() + 1 }
+          val c = Rx{ changes += "c"; a() + b() }
+          assert(changes == "bc")
+          a() = 4
+          assert(changes == "bcbc")
+        }
+        "options"-{
+          val a = Var[Option[Int]](None)
+          val b = Var[Option[Int]](None)
+          val c = Rx {
+            a().flatMap{ x =>
+              b().map{ y =>
+                x + y
+              }
+            }
+          }
+          a() = Some(1)
+          b() = Some(2)
+          assert (c.now == Some(3))
+        }
+        "rxcreate"-{
+          val trigger = Var(5)
+          val a = Rx.create[Int](0) { write =>
+            trigger.triggerLater {
+              write() = trigger.now
+            }
+          }
+
+          assert(a.now == 0)
+
+          trigger() = 7
+          assert(a.now == 7)
+        }
+      }
+      "languageFeatures" - {
+        "patternMatching" - {
+          val a = Var(1); val b = Var(2)
+          val c = Rx{
+            a() match{
+              case 0 => b()
+              case x => x
+            }
+          }
+          assert(c.now == 1)
+          a() = 0
+          assert(c.now == 2)
+        }
+        "implicitConversions" - {
+          val a = Var(1); val b = Var(2)
+          val c = Rx{
+            a() to b()
+          }
+          assert(c.now == (1 to 2))
+          a() = 0
+          assert(c.now == (0 to 2))
+        }
+        "useInByNameParameters" - {
+          val a = Var(1)
+
+          val b = Rx{ Some(1).getOrElse(a()) }
+          assert(b.now == 1)
+        }
+      }
+    }
+
+    "obsTests" - {
+      "helloWorld" - {
+        val a = Var(1)
+        var count = 0
+        val o = a.trigger{
+          count = a.now + 1
+        }
+        assert(count == 2)
+        a() = 4
+        assert(count == 5)
+      }
+      "helloWorld2" - {
+        val a = Var(1)
+        var count = 0
+        val o = a.trigger{ a =>
+          count = a + 1
+        }
+        assert(count == 2)
+        a() = 4
+        assert(count == 5)
+      }
+      "skipInitial" - {
+        val a = Var(1)
+        var count = 0
+        val o = a.triggerLater{
+          count = count + 1
+        }
+        assert(count == 0)
+        a() = 2
+        assert(count == 1)
+        a() = 2
+        assert(count == 1)
+        a() = 3
+        assert(count == 2)
+      }
+      "skipInitial2" - {
+        val a = Var(1)
+        var count = 0
+        val o = a.triggerLater{ a =>
+          count = a + 1
+        }
+        assert(count == 0)
+        a() = 2
+        assert(count == 3)
+        a() = 3
+        assert(count == 4)
+      }
+
+      "simpleExample" - {
+        val a = Var(1)
+        val b = Rx{ a() * 2 }
+        val c = Rx{ a() + 1 }
+        val d = Rx{ b() + c() }
+        var bS = 0;     val bO = b.trigger{ bS += 1 }
+        var cS = 0;     val cO = c.trigger{ cS += 1 }
+        var dS = 0;     val dO = d.trigger{ dS += 1 }
+
+        assert(bS == 1);   assert(cS == 1);   assert(dS == 1)
+
+        a() = 2
+        assert(bS == 2);   assert(cS == 2);   assert(dS == 2)
+
+        a() = 1
+        assert(bS == 3);   assert(cS == 3);   assert(dS == 3)
+      }
+      "killing" - {
+        val a = Var(1)
+        var i = 0
+        val o = a.trigger(i += 1)
+        assert(
+          a.observers.size == 1,
+          i == 1
+        )
+        a() = 2
+        assert(i == 2)
+        o.kill()
+        a() = 3
+        assert(
+          a.observers.size == 0,
+          i == 2
+        )
+      }
+      "killingInsideRx" - {
+        val a = Var(1)
+        var i = 0
+        Rx {
+          val o = a.trigger(i += 1)
+          assert(
+            a.observers.size == 1,
+            i == 1
+          )
+          a() = 2
+          assert(i == 2)
+          o.kill()
+          a() = 3
+          assert(
+            a.observers.size == 0,
+            i == 2
+          )
+        }
+      }
+      "killingVar" - {
+        val a = Var(1)
+        var i = 0
+        a.trigger(i += 1)
+        assert(
+          a.observers.size == 1,
+          i == 1
+        )
+        a() = 2
+        assert(i == 2)
+        a.kill()
+        a() = 3
+        assert(
+          a.observers.size == 0,
+          i == 2
+        )
+      }
+    }
+
+    "errorHandling" - {
+      "simpleCatch" - {
+        val a = Var(1L)
+        val b = Rx{ 1 / a() }
+        assert(b.now == 1)
+        assert(b.toTry == Success(1L))
+        a() = 0
+        intercept[Exception]{
+          b.now
+          ()
+        }
+        assertMatch(b.toTry){case Failure(_) =>}
+      }
+      "longChain" - {
+        val a = Var(1L)
+        val b = Var(2L)
+
+        val c = Rx{ a() / b() }
+        val d = Rx{ a() * 5 }
+        val e = Rx{ 5 / b() }
+        val f = Rx{ a() + b() + 2 }
+        val g = Rx{ f() + c() }
+
+        assertMatch(c.toTry){case Success(0)=>}
+        assertMatch(d.toTry){case Success(5)=>}
+        assertMatch(e.toTry){case Success(2)=>}
+        assertMatch(f.toTry){case Success(5)=>}
+        assertMatch(g.toTry){case Success(5)=>}
+
+        b() = 0
+
+        assertMatch(c.toTry){case Failure(_)=>}
+        assertMatch(d.toTry){case Success(5)=>}
+        assertMatch(e.toTry){case Failure(_)=>}
+        assertMatch(f.toTry){case Success(3)=>}
+        assertMatch(g.toTry){case Failure(_)=>}
+      }
+    }
+    "dontPropagateIfUnchanged" - {
+      val a = Var(1)
+      var ai = 0
+      var thing = 0
+      val b = Rx{ math.max(a(), 0) + thing }
+      var bi = 0
+      val c = Rx{ b() / 2 }
+      var ci = 0
+
+      a.trigger{ ai += 1 }
+      b.trigger{ bi += 1 }
+      c.trigger{ ci += 1 }
+      // if c doesn't change (because of rounding) don't update ci
+      assert(ai == 1, bi == 1, ci == 1)
+      a() = 0
+      assert(ai == 2, bi == 2, ci == 1)
+      a() = 1
+      assert(ai == 3, bi == 3, ci == 1)
+      // but if c changes then update ci
+      a() = 2
+      assert(ai == 4, bi == 4, ci == 2)
+
+      // if a doesn't change, don't update anything
+      a() = 2
+      assert(ai == 4, bi == 4, ci == 2)
+
+      // if b doesn't change, don't update bi or ci
+      a() = 0
+      assert(ai == 5, bi == 5, ci == 3)
+      a() = -1
+      assert(ai == 6, bi == 5, ci == 3)
+
+      // all change then all update
+      a() = 124
+      assert(ai == 7, bi == 6, ci == 4)
+
+      // recalcing with no change means no update
+      b.recalc()
+      assert(ai == 7, bi == 6, ci == 4)
+
+      // recalcing with change (for whatever reason) updates downstream
+      thing = 12
+      b.recalc()
+      assert(ai == 7, bi == 7, ci == 5)
+    }
+    "printing" - {
+      val v = Var(1).toString
+      val r = Rx(1).toString
+      assert(
+        v.startsWith("Var@"),
+        v.endsWith("(1)"),
+        r.startsWith("Rx@"),
+        r.endsWith("(1)")
+      )
+      (r.toString, v.toString)
+    }
+  }
+}
